85cf291f5d13dfae30da4574da945a0b0626e3db
krieger27 <krieger@vols.utk.edu>
2017-12-21 12:38:37 -0600

    upperdone

--- array-methods.js
+++ array-methods.js
@@ -1,9 +1,4 @@
-console.log("hello");
 
-var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
-var el = document.getElementById("planets");
-
-planets.forEach(element => el.innerHTML)
 /*
  Use the forEach method to add the name of each planet
  to a div element in your HTML with an id of "planets".
@@ -15,4 +10,20 @@ planets.forEach(element => el.innerHTML)
 
 // Use the reduce method to create a sentence from the words in the following array
 
-var words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"];
+    var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+
+    var el = document.getElementById("planets");
+   
+   allPlanets = planets.forEach (function(planet){
+       el.innerHTML +=`${planet} `;
+   })
+
+   let planetsUpper = planets.map((planet) =>
+    planet.charAt(0).toUpperCase()+ planet.slice(1)
+    );
+    el.innerHTML +=planetsUpper;
+
+
+// Use the reduce method to create a sentence from the words in the following array
+
+// var words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"]
